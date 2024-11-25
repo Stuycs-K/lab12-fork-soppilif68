75478ae3984df4263b5d9e8fa67e06024fa7d786
Filippos Kogiantis <filipposlk15@gmail.com>
2024-11-25 18:50:32 -0500

    got the random generation working, and with the correct number of sub-processes

--- main.c
+++ main.c
@@ -16,24 +16,46 @@ int err(){
     exit(1);
 }
 
+int * get_random(int length){
+  int r_file = open("/dev/random", O_RDONLY , 0);
+  
+  int * buff = (int *) malloc(length*sizeof(int));
+  if(r_file == -1) err();
+  int n;
+  n = read(r_file, buff, length*sizeof(int));
+  return buff;
+}
+
 int func();
 int func(){
   pid_t p = 1;
   //for generalization
+//  printf("\ninit p: %d",p);
+  
+  
   for (int i = 0;i<2;i++){ //creates the children
-    printf("\np: %d",p);
-    if (p != 0 && p != 1){ //if process is the parent, fork again
+    if (p != 0){ //if process is the parent, fork again
       p = fork();
-      printf("\nfork created");
+//      printf("\nfork created");
       if (p == -1){
         err();
       }
     }
+//    printf("\np: %d, %d",p,getpid());
   }
+  int * rp = get_random(1);
+  int r = *rp;
+  r = abs(r);
+  r = (r % 5) + 1;
+  printf("\nr:%d",r);
+  
+//  printf("\n1");
   return 0;
 }
 
 int main(){
   func();
+  
+  printf("\n");
   return 0;
 }
